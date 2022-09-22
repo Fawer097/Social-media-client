789a00ae78e7f3039200d8072d66ecd7e4be42b0
Fawer097 <domnenko.97@mail.ru>
2022-09-22 13:47:57 +0300

    final commit

--- src/http/index.js
+++ src/http/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:30001/api/';
+export const API_URL = 'http://localhost:3001/api/';
 
 export const $api = axios.create({
   withCredentials: true,
